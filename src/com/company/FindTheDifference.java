38d4811008495498e371cf92f749dccba1882256
paulchiang <paul.chiang@live.com>
2020-12-23 13:49:22 +0800

    add

--- src/com/company/FindTheDifference.java
+++ src/com/company/FindTheDifference.java
@@ -6,7 +6,20 @@ import java.util.Map;
 public class FindTheDifference {
     // https://leetcode-cn.com/problems/find-the-difference/
     public static void main(String[] args) {
-        System.out.println(findTheDifference("abc","abdc"));
+        System.out.println(findTheDifference2("abc", "abdc"));
+    }
+
+    public static char findTheDifference2(String s, String t) {
+        int sum1 = 0;
+        int sum2 = 0;
+        for (char c : s.toCharArray()) {
+            sum1 += c;
+        }
+
+        for (char c : t.toCharArray()) {
+            sum2 += c;
+        }
+        return (char) (sum2 - sum1);
     }
 
     public static char findTheDifference(String s, String t) {
