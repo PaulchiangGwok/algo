71ecb7021c1db9325e33229eb4d16fd61e7d1cc8
paulchiang <paul.chiang@live.com>
2021-01-28 17:03:07 +0800

    fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
         //int[] arr = {5,2,3};
         //int target = 10;
 
-        //System.out.println(findBestValue(arr,target));
+        System.out.println("Hello Worlds");
     }
 
 
