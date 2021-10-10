2200a12445aab103aa21d520ce1cdcdcdc99ec66
1 <paul.chiang@live.com>
2021-10-10 12:24:18 +0800

    123

--- src/com/company/LongestPalindrome.java
+++ src/com/company/LongestPalindrome.java
@@ -5,7 +5,7 @@ package com.company;
  */
 public class LongestPalindrome {
     public static void main(String[] args) {
-        System.out.println(longestPalindrome("ac"));
+        System.out.println(longestPalindrome("cc"));
     }
 
     public static String longestPalindrome(String s) {
@@ -13,8 +13,8 @@ public class LongestPalindrome {
             return s;
         }
 
-        if(s.length() == 2){
-            if(s.charAt(0) == s.charAt(1)){
+        if (s.length() == 2) {
+            if (s.charAt(0) == s.charAt(1)) {
                 return s;
             } else {
                 return String.valueOf(s.charAt(0));
@@ -23,18 +23,18 @@ public class LongestPalindrome {
 
         String res = "";
         //边界的两个不需要判断
-        for (int i = 1; i < s.length() - 2; i++) {
+        for (int i = 0; i < s.length() - 1; i++) {
             //先判断是中心对称还是轴对称
             String tmp;
             if (s.charAt(i) == s.charAt(i + 1)) {
                 //中心对称
                 tmp = check(s, i, i + 1);
-            } else {
-                //测试轴对称
-                tmp = check(s, i, i);
-
             }
-            if(tmp.length() > res.length()){
+            //测试轴对称
+            tmp = check(s, i, i);
+
+
+            if (tmp.length() > res.length()) {
                 res = tmp;
             }
         }
@@ -42,7 +42,7 @@ public class LongestPalindrome {
     }
 
     public static String check(String s, int start, int end) {
-        while (start > 0 && end < s.length()) {
+        while (start > 0 && end < s.length() - 1) {
             if (s.charAt(start - 1) == s.charAt(end + 1)) {
                 start--;
                 end++;
